6cd35ce660ad63f97c34779f20a1817a60c70627
mwaiyusuf <mwaigalo5@gmail.com>
2019-02-15 22:25:07 +0300

    calculate

--- js/script.js
+++ js/script.js
@@ -1,27 +1,40 @@
 function triangleTracker() {
   /*since every input is an strings,av to convert to integer*/
-  var sideQ = parseInt(document.getElementbyId('q').value);
-  var sideR = parseInt(document.getElementbyId('r').value);
-  var sideS = parseInt(document.getElementbyId('s').value);
+  var sideQ = parseInt(document.getElementById('q').value);
+  var sideR = parseInt(document.getElementById('r').value);
+  var sideS = parseInt(document.getElementById('s').value);
+  // var sideQ=sideR=sideS = 6;
   /* creating a variable to store the types of triangle*/
   var triangle=["Equalateral", "isosceles", "scalene"];
+  var response=document.getElementById('response');
   /*the major condition for a triangle to be constructed*/
+
   if (sideQ + sideR > sideS && sideR + sideS > sideQ && sideQ + sideS > sideR) {
 
     /*if it is a triangle,now to check type of the triangle*/
-    else if (sideQ == sideR && sideR == sideS) {
-      document.getElementbyId('response').innerHTML = 'this is an equilateral';
+    if (sideQ == sideR && sideR == sideS && sideQ==sideS) {
+      // alert('this is an equilateral');
+      response.innerHTML = "<h1>this is an equilateral triangle</h1>"
+
+
     }
     /*if its not an equalateral to check for another type */
-    else if (sideQ != sideR && sideR != sideS) {
-      alert('this is a scalene');
+    else if (sideQ != sideR && sideR != sideS && sideQ!=sideS) {
+
+      response.innerHTML = "<p>this is an scalene triangle</p>"
+
     }
     /*last condition for a triangle*/
-    else if {
-      alert('this is a isosceles');
+    else  {
+
+      response.innerHTML = "<p>this is isosceles triangle</p>"
+
     }
+}
+  /*if someone inputs something that is not a number to output this*/
+  else if(isNaN(sideQ) || isNaN(sideR) || isNaN(sideS) ){
+     alert('please enter a number');
   }
-  /*if someone inputs something that is not a number to out this*/
   else {
     alert('This is not a triangle');
   }
