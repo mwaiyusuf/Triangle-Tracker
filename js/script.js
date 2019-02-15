8e3680875d9beaec137763022455d208ba64956c
mwaiyusuf <mwaigalo5@gmail.com>
2019-02-15 17:42:08 +0300

    index.html

--- js/script.js
+++ js/script.js
@@ -1,32 +1,29 @@
-function triangleTracker(){
-/*since every input is an strings,av to convert to integer*/
-var sideQ=parseInt(document.getElementbyId('q'));
-var sideR=parseInt(document.getElementbyId('r'));
-var sideS=parseInt(document.getElementbyId('s'));
-/* creating a variable to store the types of triangle*/
-var triangle=["Equalateral", "isosceles", "scalene"];
-/*the major condition for a triangle to be constructed*/
-if(sideQ+sideR>sideS || sideR+sideS>sideQ || sideQ+sideS>sideR){
-  /*if it is a triangle,now to check type of the triangle*/
-  if(sideQ==sideR && sideR==sideS){
-    alert('this is an equilateral');
+function triangleTracker() {
+  /*since every input is an strings,av to convert to integer*/
+  var sideQ = parseInt(document.getElementbyId('q').value);
+  var sideR = parseInt(document.getElementbyId('r').value);
+  var sideS = parseInt(document.getElementbyId('s').value);
+  /* creating a variable to store the types of triangle*/
+  var triangle=["Equalateral", "isosceles", "scalene"];
+  /*the major condition for a triangle to be constructed*/
+  if (sideQ + sideR > sideS && sideR + sideS > sideQ && sideQ + sideS > sideR) {
+
+    /*if it is a triangle,now to check type of the triangle*/
+    if (sideQ == sideR && sideR == sideS) {
+      document.getElementbyId('response').innerHTML = 'this is an equilateral';
+    }
+    /*if its not an equalateral to check for another type */
+    else if (sideQ != sideR && sideR != sideS) {
+      alert('this is a scalene');
+    }
+    /*last condition for a triangle*/
+    else {
+      alert('this is a isosceles');
+    }
   }
-  /*if its not an equalateral to check for another type */
-  else if(sideQ!=sideR || sideR!=sideS){
-    alert('this is a scalene');
+  /*if someone inputs something that is not a number to out this*/
+  else {
+    alert('This is not a triangle');
   }
-  /*last condition for a triangle*/
-  else{
-    alert('this is a isosceles');
-  }
-}
-/*if someone inputs something that is not a number to out this*/
-else if(sideQ==isNaN || sideR==isNaN || sideS==isNaN){
-  alert('Enter a number');
-}
-else{
-  alert('This is not a triangle');
-}
 
 }
-triangleTracker();
