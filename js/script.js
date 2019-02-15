0d854745979a7b938e36f6775d88a6f8d0f11974
mwaiyusuf <mwaigalo5@gmail.com>
2019-02-15 15:58:42 +0300

    opening a function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
+function triangleTracker(){
 /*since every input is an strings,av to convert to integer*/
 var sideq=parseInt(document.getElementbyId('q'));
 var sider=parseInt(document.getElementbyId('r'));
 var sides=parseInt(document.getElementbyId('s'));
 /* creating a variable to store the types of triangle*/
+var triangle=["Equalateral", "isosceles", "scalene"];
+/*the major condition for a triangle to be constructed*/
+if(sideQ+sideR>sideS || sideR+sideS>sideQ || sideQ+sideS>sideR){
+  if(sideQ==sideR && sideR==sideS){
+    .innerHTML('this is an equilateral');
+  }
+  // else if(sideQ!=sideR || sideR!=sideS){
+  //   .innerHTML('this is a scalene');
+  // }
+//   else{
+//     .innerHTML('this is a isosceles');
+//   }
+// }
+// else if(sideQ==isNaN || sideR==isNaN || sideS==isNaN){
+//   .innerHTML('Enter a number');
+// }
+// else{
+//   .innerHTML('This is not a triangle');
+// }
+//
+// }
+// triangleTracker();
