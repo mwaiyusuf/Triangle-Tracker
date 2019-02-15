8367e087a94579ff7e179bd1b08d447e83546d9d
mwaiyusuf <mwaigalo5@gmail.com>
2019-02-15 15:46:20 +0300

    created varibale to parseint the inputs

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*since every input is an strings,av to convert to integer*/
+var sideq=parseInt(document.getElementbyId('q'));
+var sider=parseInt(document.getElementbyId('r'));
+var sides=parseInt(document.getElementbyId('s'));
+/* creating a variable to store the types of triangle*/
